3ac67faabc9df1be080b64a4bbbe6c0ea346e537
LEI-122695 <matildegit5@gmail.com>
2025-11-18 13:07:09 +0000

    add CompassTest class with initial test methods

---
+++ src/test/java/iscteiul/ista/battleship/CompassTest.java
@@ -0,0 +1,28 @@
+package iscteiul.ista.battleship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompassTest {
+
+    @Test
+    void getDirection() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void charToCompass() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+}
